127dea7625de91a7007389006e1de95b52942840
juanescobar1410 <escobarsanchezjuanandres@gmail.com>
2025-10-01 15:57:16 -0500

    Personaje camina

---
+++ Taller2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,73 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float jumpForce;
+    public int MaxJumps;
+    public LayerMask FloorLayer;
+
+    private new Rigidbody2D rigidbody;
+    private BoxCollider2D boxCollider;
+    private bool LookingRight = true;
+    private int JumpCount;
+
+
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        boxCollider = GetComponent<BoxCollider2D>();
+        JumpCount = MaxJumps;
+    }
+    void Update()
+    {
+        MovementProccess();
+        JumpProccess();
+    }
+
+    bool itsinFloor()
+    {
+        RaycastHit2D raycast = Physics2D.BoxCast(boxCollider.bounds.center, new Vector2(boxCollider.bounds.size.x, boxCollider.bounds.size.y), 0f, Vector2.down, 0.1f, FloorLayer);
+        return raycast.collider != null;
+    }
+    void JumpProccess()
+    {
+
+        if (itsinFloor())
+        {
+            JumpCount = MaxJumps;
+
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.Space) && JumpCount > 0)
+        {
+            JumpCount--;
+            rigidbody.linearVelocity = new Vector2(rigidbody.linearVelocity.x, 0f);
+            rigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
+
+    void MovementProccess()
+    {
+        float InputMovement = Input.GetAxis("Horizontal");
+
+        rigidbody.linearVelocity = new Vector2(InputMovement * speed, rigidbody.linearVelocity.y);
+
+        ManageOrientation(InputMovement);
+    }
+    
+    void ManageOrientation(float InputMovement)
+    {
+        if ((LookingRight == true && InputMovement < 0) || (LookingRight == false && InputMovement > 0))
+        {
+            LookingRight = !LookingRight;
+            transform.localScale = new Vector2(-transform.localScale.x, transform.localScale.y);
+        }
+    }
+}
+
+
