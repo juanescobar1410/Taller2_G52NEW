6d7ba853c735fb89d0aeb0c770433745792e8cce
catalinaortiz-hub <catalina.ortiz@uao.edu.co>
2025-10-02 21:36:06 -0500

    Monedas en el HUD

---
+++ Taller2/Assets/Scripts/Scene2/GameControllerScene2.cs
@@ -0,0 +1,71 @@
+using UnityEngine;
+
+public class GameControllerScene2 : MonoBehaviour
+{
+    public Timer tiempoEscena;
+
+
+
+
+    public void GuardarTiempoEscena()
+    {
+        // Accedemos al stopTime
+        tiempoEscena.TimerStop();
+        float timeScene2 = tiempoEscena.StopTime1;
+        // Lo mandamos al GameManager
+        GameManager.Instance.SumaTimeGlobal(timeScene2);
+
+        Debug.Log("Tiempo de esta escena: " + timeScene2);
+        //Debug.Log("Tiempo global acumulado: " + GameManager.Instance.Globaltime1);
+
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+
+            GuardarTiempoEscena();
+            Time.timeScale = 0f;
+
+            //panelResultados.SetActive(true);
+
+            //ActualizarResultados();
+
+            // Mostrar resultados en consola también
+            //Debug.Log($"Resultados: Cerezas={GameManager.Instance.Cerezas}, Sandías={GameManager.Instance.Sandias}");
+        }
+    }
+    //public void ActualizarResultados()
+    //{
+    //    if (GameManager.Instance != null)
+    //    {
+    //        // Lee los contadores de GameManager
+    //        int totalCerezas = GameManager.Instance.Cerezas;
+    //        int totalSandias = GameManager.Instance.Sandias;
+    //        float tiempo = GameManager.Instance.Globaltime1;
+
+    //        // Muestra en los textos
+    //        txtCerezas.text = totalCerezas.ToString();
+    //        txtSandias.text = totalSandias.ToString();
+    //        txtTiempoGlobal.text = tiempo.ToString("F2");
+
+    //        Debug.Log("Resultados cargados en el panel");
+    //    }
+    //    else
+    //    {
+    //        Debug.LogError(" No existe GameManager en la escena.");
+    //    }
+    //}
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
