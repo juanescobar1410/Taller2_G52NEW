1db99fa7c8f6af244bbff4f1999a054165645cc8
Fanieruao <163151143+Fanieruao@users.noreply.github.com>
2025-10-03 20:44:42 -0500

    Acomode el HUD y organize algunas cosas

---
+++ Taller2/Assets/Scripts/PlayerScripts/Enemy.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            GameManager.Instance.PerderVida();
+        }
+    }
+
+
+}
